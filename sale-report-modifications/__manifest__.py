e05739d861de3789cf6498a068a5c6666d0c6dbd
rsullivan2704 <rsullivan2704@gmail.com>
2018-01-20 16:28:26 -0500

    Feature sale report mods (#1)
    * created module to modify sale reports
    updated report_saleorder_document to reformat the display of addresses.
    
    * changed link for issues.

---
+++ sale-report-modifications/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2018 Robert J Sullivan
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Report Modifications",
+    "summary": "Modifications to Standard Odoo Sales Reports",
+    "version": "10.0.1.0.0",
+    "depends": ["sale"],
+    "author": "Robert J Sullivan",
+    "license": "AGPL-3",
+    "data": [
+        'views/report_saleorder.xml',
+    ],
+    "category": "Sale",
+    'installable': True,
+}
